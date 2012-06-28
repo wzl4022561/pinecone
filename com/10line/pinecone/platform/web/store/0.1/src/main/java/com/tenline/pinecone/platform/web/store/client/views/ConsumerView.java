f262aaab3d01cafff8a71daf52c6081c2639c994
godenlue <godenlue@gmail.com>
2012-06-28 11:17:54 +0000

    增加了ConsumerEvents.GET_ALL的事件处理

--- com/10line/pinecone/platform/web/store/0.1/src/main/java/com/tenline/pinecone/platform/web/store/client/views/ConsumerView.java
+++ com/10line/pinecone/platform/web/store/0.1/src/main/java/com/tenline/pinecone/platform/web/store/client/views/ConsumerView.java
@@ -3,16 +3,32 @@
  */
 package com.tenline.pinecone.platform.web.store.client.views;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.extjs.gxt.ui.client.Registry;
+import com.extjs.gxt.ui.client.data.BeanModel;
+import com.extjs.gxt.ui.client.data.BeanModelFactory;
+import com.extjs.gxt.ui.client.data.BeanModelLookup;
 import com.extjs.gxt.ui.client.mvc.AppEvent;
 import com.extjs.gxt.ui.client.mvc.Controller;
 import com.extjs.gxt.ui.client.mvc.View;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.tenline.pinecone.platform.model.Consumer;
+import com.tenline.pinecone.platform.web.store.client.events.ConsumerEvents;
+import com.tenline.pinecone.platform.web.store.client.widgets.AppStoreViewPort;
 
 /**
  * @author Bill
  *
  */
 public class ConsumerView extends View {
-
+	/**
+	 * 创建Consumer转换为BeanMode的工厂实例
+	 * @author lue
+	 */
+	private BeanModelFactory consumerFactory = BeanModelLookup.get().getFactory(Consumer.class);
 	/**
 	 * @param controller
 	 */
@@ -23,8 +39,32 @@ public class ConsumerView extends View {
 
 	@Override
 	protected void handleEvent(AppEvent event) {
-		// TODO Auto-generated method stub
+		//add by lue at 2012年6月22日17:06:52
+		if(event.getType().equals(ConsumerEvents.GET_ALL)) {
+			RootPanel.get().clear();
+			updateAppStoreGrid(event);
+		} else if(event.getType().equals(ConsumerEvents.GET_BY_CATEGORY)) {
+			updateAppStoreGrid(event);
+		} else if(event.getType().equals(ConsumerEvents.REGISTER)) {
+			System.out.println(event.getData().getClass());
+		}
+	}
 
+	@SuppressWarnings("unchecked")
+	private void updateAppStoreGrid(AppEvent event) {
+		AppStoreViewPort appStoreViewPort = Registry.get(AppStoreViewPort.class.getName());
+		RootPanel.get().add(appStoreViewPort);
+		List<BeanModel> data = new ArrayList<BeanModel>();
+		if(event.getData() != null) {
+			for(Consumer c : (Collection<Consumer>)event.getData()) {
+				BeanModel bean = consumerFactory.createModel(c);
+				bean.set("detail", c.getName());
+				data.add(bean);
+			}
+//			System.out.println(event.getData().getClass());
+			System.out.println(((ArrayList<Consumer>)event.getData()).size());
+		}
+		appStoreViewPort.setAppGridData(data);
 	}
 
 }
